534628e22c57216fea470563a383354fcf8bc66e
강진우 <wlsdn1252@naver.com>
2022-12-29 20:13:52 +0900

    버튼 클릭시 로그 찍어보기

--- app/src/main/java/com/example/kotlinbasic01/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbasic01/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.kotlinbasic01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +11,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        clickBtn.setOnClickListener {
+            Log.d("메인화면", "버튼이 클릭되었습니다.") // 버튼이 클릭됐을때 로그 출력하기
+        }
 
     }
 }
